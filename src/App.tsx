66264f3d5f652427d2fbf965f43e74aa2cf78c1b
Kelvin Yip <kelvinyip7@Kelvins-MacBook-Pro.local>
2022-09-01 17:02:44 -0400

    React Version of Redesigned Website. Removed 5615920 per internal convo.

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import './App.css';
+import { ABOUTUS, BOARD, PROJECTS } from './data';
+import logo from './assets/ycs_black.png'
+
+import email from './assets/email.svg'
+import facebook from './assets/facebook.svg'
+import instagram from './assets/instagram.svg'
+import people from './assets/people.jpeg'
+
+function App() {
+    return (
+        <div className="App">
+            <div style={{ backgroundColor: '#1f232b' }}>
+                <img src={logo} alt={"Yale Computer Society logo"} style={{ maxHeight: '200px' }} />
+            </div>
+            <div style={{ backgroundColor: '#1f232b', color: 'white', padding: '0 0 2rem 0', display: "flex", flexDirection: 'column', alignItems: 'center' }}>
+                <h3>Students promoting tech &amp; entrepreneurship at Yale University</h3>
+                <div style={{ cursor: "pointer", border: "5px solid white", borderRadius: '15px', width: "fit-content", padding: '15px' }} onClick={() => window.open("https://yalecompsociety.us7.list-manage.com/subscribe/post?u=83b3abd7222875600c46cf8de&amp;id=155d6c54ff", "_blank")} >
+                    Subscribe To Our Mailchimp
+                </div>
+            </div>
+            <h1>About Us</h1>
+            <h4>What We Do</h4>
+            <div className='card-grid'>
+                {
+                    ABOUTUS.map((about, i) => (
+                        <div className="card" key={i}>
+                            <img src={about.icon} alt={about.name} style={{ width: '50px', height: '50px' }} />
+                            <strong style={{ margin: '1rem 0' }}>{about.name}</strong>
+                            <p>{about.description}</p>
+                        </div>
+                    ))
+                }
+            </div>
+            <h1>Fall 2022 Development Projects</h1>
+            <h4>Here is the software we are building to improve the Yale experience using technology!</h4>
+            <div className='card-grid'>
+                {
+                    PROJECTS.map((project, i) => (
+                        <div className="card" key={i}>
+                            <div style={{ width: '150px', height: '150px', display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                                <img src={project.image} alt={project.name} style={{ maxWidth: '150px', maxHeight: '150px', borderRadius: '15px' }}/>
+                            </div>
+                            <strong style={{ marginTop: '10px' }}>
+                                {project.name}
+                            </strong>
+                        </div>
+                    ))
+                }
+            </div>
+            <h1>Fall 2022 Board</h1>
+            <h4>Meet the people behind the mission!</h4>
+            <div className="card-grid">
+                {
+                    BOARD.map((board_member, i) => (
+                        <div className="card" key={i}>
+                            <img src={people} alt={board_member.name} style={{ width: '100%', maxWidth: '200px', marginBottom: '1rem', height: '100%', maxHeight: '200px' }}/>
+                            <strong>{board_member.name}</strong>
+                            <p>{board_member.role}</p>
+                        </div>
+                    ))
+                }
+            </div>
+            <footer style={{ backgroundColor: '#1f232b', padding: '15px', display: 'flex', flexDirection: 'column', color: 'white' }}>
+                <div>
+                <a href="mailto:yalecomputersociety@gmail.com">
+                    <img src={email} className="social-logo" style={{ marginRight: '10px' }} alt="Email"/>
+                </a>
+                <a href="https://www.facebook.com/yalecompsociety/" target="_blank" rel="noreferrer">
+                    <img className="social-logo" style={{ margin: '0 10px' }} src={facebook} alt="Facebook"/>
+                </a>
+                <a href="https://www.instagram.com/yalecompsociety/" target="_blank" rel="noreferrer">
+                    <img className="social-logo" style={{ marginLeft: '10px' }} src={instagram} alt="Instagram"/>
+                </a>
+                </div>
+                <div style={{ marginTop: '5px' }}>
+                    Copyright 2022
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default App;
--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Product imports
 import { StaticImageData } from "next/image";
 
@@ -364,3 +365,124 @@ export const PARTNERSHIPS = [
     image: tsai,
   },
 ];
+=======
+import coursetable from './assets/coursetable.svg'
+import yalies from './assets/yalies.png'
+import yuts from './assets/yuts-square.png'
+import campushub from './assets/campushub.png'
+import mealmatch from './assets/MealMatch.png'
+
+import dev_projects from './assets/code.svg'
+import networking_events from './assets/briefcase.svg'
+import hack_nights from './assets/terminal.svg'
+import socials from './assets/user.svg'
+
+export const BOARD = [
+    {
+        name: "Kelvin Yip",
+        image: "",
+        role: "President"
+    },
+    {
+        name: "Kyle Andruczk",
+        image: "",
+        role: "Vice President, Yuts Project Lead"
+    },
+    {
+        name: "Meili Gupta",
+        image: "",
+        role: "Secretary, Events Team Lead"
+    }, 
+    {
+        name: "Addison Goolsbee",
+        image: "",
+        role: "Treasurer"
+    },
+    {
+        name: "Brandon Yee",
+        image: "",
+        role: "Social Chair"
+    },
+    {
+        name: "Rosie Rothschild",
+        image: "",
+        role: "Outreach Chair"
+    },
+    {
+        name: "Andy Wu",
+        image: "",
+        role: "Advisor"
+    },
+    {
+        name: "Lily Zhou",
+        image: "",
+        role: "CourseTable Project Lead"
+    },
+    {
+        name: "Lucas Huang",
+        image: "",
+        role: "CourseTable Project Lead"
+    },
+    {
+        name: "Erik Boesen",
+        image: "",
+        role: "Yalies Project Lead"
+    },
+    {
+        name: "Tucker Moses",
+        image: "",
+        role: "CampusHub Project Lead"
+    },
+    {
+        name: "Kai Xu",
+        image: "",
+        role: "MealMatch Project Lead"
+    }
+]
+
+export const PROJECTS = [
+    {
+        name: "CourseTable",
+        image: coursetable
+    },
+    {
+        name: "Yalies",
+        image: yalies
+    },
+    {
+        name: "Yuts",
+        image: yuts
+    },
+    {
+        name: "CampusHub",
+        image: campushub
+    },
+    {
+        name: "MealMatch",
+        image: mealmatch
+    }
+]
+
+export const ABOUTUS = [
+    {
+        name: "Development Projects",
+        icon: dev_projects,
+        description: "We offer development projects to gain real-world software engineering experience."
+    },
+    {
+        name: "Speaker Events",
+        icon: networking_events,
+        description: "We host speaker events with technologists and industry professionals."
+    },
+    {
+        name: "Hack Nights",
+        icon: hack_nights,
+        description: "We host Hack Nights for students to work together and hack away at projects of their choosing."
+    },
+    {
+        name: "Social Events",
+        icon: socials,
+        description: "We host social events for CS students to meet each other and exchange interests."
+    }
+]
+>>>>>>> 2b93c9e (React Version of Redesigned Website. Removed 5615920 per internal convo.)
