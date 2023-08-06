c236196fbbea33eeb9e042d3c23ce2106c0beb0c
Kelvin Yip <kelvinyip7@Kelvins-MBP.home>
2023-08-06 03:58:54 -0400

    Add documentation to General Product template, clean up with CSS classes

--- src/pages/GeneralProduct.tsx
+++ src/pages/GeneralProduct.tsx
@@ -2,11 +2,9 @@ import './GeneralProduct.css'
 
 interface GeneralProductProps {
     productName: string;
-    productLogo: string;
     productSlogan: string;
     productColor: string;
     isWeb: boolean;
-    headerImage: string;
     firstSectionTitle: string;
     firstSectionText: (string | JSX.Element)[] | string;
     secondSectionTitle: string;
@@ -15,9 +13,22 @@ interface GeneralProductProps {
     thirdSectionText: (string | JSX.Element)[] | string;
 }
 
+/**
+ * This component serves as a template for all product pages to be displayed on the Yale Computer Society Website.
+ * @param {string} productName The name of the product. The spelling of the product is important as the app's assets will be loaded by injecting this name into the path.
+ * @param {string} productSlogan The slogan of the product. What is displayed right under the name of the product in the product's main section.
+ * @param {string} productColor The HEX color code of the product.
+ * @param {boolean} isWeb Whether or not the product is a web app or not.
+ * @param {string} firstSectionTitle The title to display in the first section of the product description.
+ * @param {(string | JSX.Element)[] | string} firstSectionText The text to display in the first section of the product description.
+ * @param {string} secondSectionTitle The title to display in the second section of the product description.
+ * @param {(string | JSX.Element)[] | string} secondSectionText The text to display in the second section of the product description.
+ *  * @param {string} thirdSectionTitle The title to display in the third section of the product description.
+ * @param {(string | JSX.Element)[] | string} thirdSectionText The text to display in the third section of the product description.
+ * @returns {JSX.Element}
+ */
 export const GeneralProduct = ({
     productName,
-    productLogo,
     productSlogan,
     productColor,
     isWeb,
@@ -27,59 +38,63 @@ export const GeneralProduct = ({
     secondSectionText,
     thirdSectionTitle,
     thirdSectionText
-}: GeneralProductProps) => {
+}: GeneralProductProps): JSX.Element => {
+
+    interface GeneralProductSectionImageProps {
+        sectionNumber: string;
+    }
+    
+    const GeneralProductSectionImage = ({ sectionNumber }: GeneralProductSectionImageProps) => (
+        <div className='section-image-container'>
+            <img loading='lazy' src={require(`../assets/products/${productName}/${sectionNumber}sectionimage.png`)} className='section-image' style={{ border: `10px solid ${productColor}` }} alt={`Product ${sectionNumber} Section Screenshot`}/>
+        </div>
+    )
+
+    interface GeneralProductSectionTextProps {
+        title: string;
+        text: (string | JSX.Element)[] | string;
+        align: CanvasTextAlign;
+    }
+
+    const GeneralProductSectionText = ({ title, text, align }: GeneralProductSectionTextProps) => (
+        <div className='section-text-container'>
+            <div className='text-container'>
+                <div className='section-text-title' style={{ textAlign: align }}>{title}</div>
+                <div className='section-text' style={{ textAlign: align }}>{text}</div>
+            </div>
+        </div>
+    )
 
     return (
         <div style={{ paddingTop: '10vh' }}>
-            <div style={{ backgroundColor: "#1F232C", padding: "20px 0", height: "90vh", display: "flex" }}>
-                <div style={{ width: "40%", display: "flex", height: "100%", flexDirection: "column", alignItems: 'center', justifyContent: "space-around" }}>
-                    <div style={{ display: "flex", height: "60%", flexDirection: "column", alignItems: 'center', justifyContent: "space-around", width: "90%" }}>
-                        <div style={{ display: "flex" }}>
-                            <div style={{ fontWeight: 700, color: "white", fontSize: "57px" }}>{productName}</div> 
-                            <img src={require(`../assets/products/${productName}/productlogo.png`)} alt={productName} style={{ width: "67.5px", height: "67.5px", marginLeft: "1rem" }} />
+            <div style={{ background: "#1F232C", padding: "20px 0", height: "90vh", display: "flex" }}>
+                <div className='section-text-container'>
+                    <div className='text-container' style={{ width: "75%" }}>
+                        <div style={{ display: "flex", width: "100%" }}>
+                            <div style={{ fontWeight: 700, color: "white", fontSize: "57px", float: "left", display: "inline-block" }}>{productName}</div> 
+                            <img src={require(`../assets/products/${productName}/productlogo.png`)} alt={productName} style={{ width: "67.5px", height: "67.5px", marginLeft: "1rem", display: "inline-block" }} />
                         </div>
-                        <div style={{ fontWeight: 400, color: "white", fontSize: "32px" }}>{productSlogan}</div>
-                        <div style={{ backgroundColor: productColor, color: "white", width: "fit-content", borderRadius: "50px", padding: "25px 35px", fontWeight: 500, fontSize: "28.5px" }}>
+                        <div style={{ fontWeight: 400, color: "white", fontSize: "32px", textAlign: "left" }}>{productSlogan}</div>
+                        <div style={{ backgroundColor: productColor, color: "white", width: "fit-content", borderRadius: "50px", padding: "25px 35px", fontWeight: 500, fontSize: "28.5px", marginRight: "auto" }}>
                             {isWeb ? "Visit Site" : "Download App"}
                         </div>
                     </div>
                 </div>
-                <div style={{ width: "60%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <div className='section-image-container'>
                     <img src={require(`../assets/products/${productName}/headerimage.png`)} style={{ width: "95%" }} alt="Product Main Screenshot"/>
                 </div>
             </div>
             <div className='first-section' style={{ padding: "20px 0", height: "90vh", display: "flex" }}>
-                <div style={{ width: "60%", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    <img src={require(`../assets/products/${productName}/firstsectionimage.png`)} style={{ width: "90%", borderRadius: 50, border: `10px solid ${productColor}` }} alt="Product First Section Screenshot"/>
-                </div>
-                <div style={{ width: "40%", display: "flex", height: "100%", flexDirection: "column", alignItems: 'center', justifyContent: "space-around" }}>
-                    <div style={{ display: "flex", height: "60%", flexDirection: "column", alignItems: 'center', justifyContent: "space-around", width: "90%" }}>
-                        <div style={{ fontWeight: "bold", fontSize: "48px", color: "white" }}>{firstSectionTitle}</div>
-                        <div style={{ fontSize: "36px", color: "white", whiteSpace: "pre-wrap", textAlign: "right" }}>{firstSectionText}</div>
-                    </div>
-                </div>
+                <GeneralProductSectionImage sectionNumber='first'/>
+                <GeneralProductSectionText title={firstSectionTitle} text={firstSectionText} align='right'/>
             </div>
-            <div style={{ backgroundColor: productColor, padding: "20px 0", height: "90vh", display: "flex" }}>
-                <div style={{ width: "40%", display: "flex", height: "100%", flexDirection: "column", alignItems: 'center', justifyContent: "space-around" }}>
-                    <div style={{ display: "flex", height: "60%", flexDirection: "column", alignItems: 'center', justifyContent: "space-around", width: "90%" }}>
-                        <div style={{ fontWeight: "bold", fontSize: "48px", color: "white" }}>{secondSectionTitle}</div>
-                        <div style={{ fontSize: "36px", color: "white", whiteSpace: "pre-wrap", textAlign: "left" }}>{secondSectionText}</div>
-                    </div>
-                </div>      
-                <div style={{ width: "60%", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    <img src={require(`../assets/products/${productName}/secondsectionimage.png`)} style={{ width: "90%", borderRadius: 50, border: `10px solid ${productColor}` }} alt="Product Second Section Screenshot"/>
-                </div>      
+            <div style={{ background: productColor, padding: "20px 0", height: "90vh", display: "flex" }}>
+                <GeneralProductSectionText title={secondSectionTitle} text={secondSectionText} align='left'/> 
+                <GeneralProductSectionImage sectionNumber='second'/>
             </div>
             <div className='third-section' style={{ padding: "20px 0", height: "90vh", display: "flex" }}>
-                <div style={{ width: "60%", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    <img src={require(`../assets/products/${productName}/thirdsectionimage.png`)} style={{ width: "90%", borderRadius: 50, border: `10px solid ${productColor}` }} alt="Product Third Section Screenshot"/>
-                </div>
-                <div style={{ width: "40%", display: "flex", height: "100%", flexDirection: "column", alignItems: 'center', justifyContent: "space-around" }}>
-                    <div style={{ display: "flex", height: "60%", flexDirection: "column", alignItems: 'center', justifyContent: "space-around", width: "90%" }}>
-                        <div style={{ fontWeight: "bold", fontSize: "48px", color: "white", textAlign: "right" }}>{thirdSectionTitle}</div>
-                        <div style={{ fontSize: "36px", color: "white", whiteSpace: "pre-wrap", textAlign: "right" }}>{thirdSectionText}</div>
-                    </div>
-                </div>
+                <GeneralProductSectionImage sectionNumber='third'/>
+                <GeneralProductSectionText title={thirdSectionTitle} text={thirdSectionText} align='right'/> 
             </div>
         </div>
     )
