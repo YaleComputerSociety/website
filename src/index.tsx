a81172dd6a0e2ae3d41633f90336a417ab2c1418
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-08-07 11:57:02 -0500

    Add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <HashRouter>
-      <App />
+    <App />
   </HashRouter>
 );
 
