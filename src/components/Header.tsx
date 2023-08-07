b34309711d77b4e448682f4388be4afc93322f6e
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-08-07 11:30:44 -0500

    Replace home page with autogenerated figma version, create new navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,24 @@
-import { Link } from 'react-router-dom'
-import ycs_logo from '../assets/ycs_logo_dark_512.png'
-import './Header.css'
+import { Link } from "react-router-dom";
+import ycs_logo from "../assets/ycs_logo_dark_512.png";
+import "./Header.css";
 
 export const Header = () => {
-
-    return (
-        <div className='header'>
-            <Link to="/">
-                <img src={ycs_logo} alt="YCS Logo" className='logo'/>
-            </Link>
-            <div style={{ float: 'right', display: 'flex', alignItems: 'center', justifyContent: 'end', height: '100%', paddingRight: '2.5vw' }}>
-                <Link className='header-link' to="/team">
-                    Team
-                </Link>
-                <Link className='header-link' to="/products">
-                    Products
-                </Link>
-                <Link className='header-link' target="_blank" to="https://airtable.com/appuVd3lWwTikuv40/shrLYVYcHUDQa8T3h">
-                    Feedback
-                </Link>
-            </div>
-        </div>
-    )
-
-}
+  return (
+    <div className="header">
+      <Link className="logo" to="/">
+        &lt;y/cs&gt;
+      </Link>
+      <div className="pagesList">
+        <Link className="header-link" to="/team">
+          Team
+        </Link>
+        <Link className="header-link" to="/products">
+          Products
+        </Link>
+        <Link className="header-link" target="_blank" to="https://airtable.com/appuVd3lWwTikuv40/shrLYVYcHUDQa8T3h">
+          Feedback
+        </Link>
+      </div>
+    </div>
+  );
+};
