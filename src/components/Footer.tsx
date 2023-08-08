1b57289c357155e9aeedde8f61ba5a1606a32ffd
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-08-08 09:30:21 -0500

    Finish home page remake

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,22 @@
-import { MEDIA } from '../data'
-import './Footer.css'
+import { MEDIA } from "../data";
 
 export const Footer = () => {
+  const linkProps = {
+    target: "_blank",
+    rel: "noreferrer",
+  };
 
-    const linkProps = {
-        target: "_blank",
-        rel: "noreferrer"
-    }
-
-    return (
-        <footer className="footer">
-            <div>
-                {
-                    MEDIA.map((social, i) => (
-                        <a key={i} href={social.link} {...(social.alt === "Email" ? {} : linkProps)}>
-                            <img 
-                                className='social-logo' 
-                                style={social.margin} 
-                                src={social.imageSrc} 
-                                alt={social.alt} 
-                                loading="lazy"
-                            />
-                        </a>
-                    ))
-                }
-            </div>
-            <div style={{ marginTop: '5px' }}>
-                Yale Computer Society © 2023
-            </div>
-        </footer>
-    )
-
-}
+  return (
+    <footer className="p-4 mt-16 flex flex-col text-white z-20">
+      <div className="text-white text-xl mb-3">Get in touch with us</div>
+      <div className="flex justify-center">
+        {MEDIA.map((social, i) => (
+          <a key={i} href={social.link} {...(social.alt === "Email" ? {} : linkProps)}>
+            <img className="h-7 transform transition-transform hover:scale-110 hover:duration-200" style={social.margin} src={social.imageSrc} alt={social.alt} loading="lazy" />
+          </a>
+        ))}
+      </div>
+      <div className="mt-4 text-gray-400">Yale Computer Society © 2023</div>
+    </footer>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,30 @@
 import { SectionTitle } from "../components/SectionTitle";
 import logo from "../assets/ycs_black.png";
 import { ABOUTUS, PROJECTS } from "../data";
-import { Fragment } from "react";
+import { Fragment, FC } from "react";
 import { CardWrapper } from "../components/CardWrapper";
 
-import React from "react";
 import "./home.css";
-import "../index.css";
+
+interface GradientBoxProps {
+  title: string;
+  text: string;
+}
+
+const GradientBox: FC<GradientBoxProps> = ({ title, text }) => {
+  return (
+    <div className="w-full h-56 text-left bg-gradient-to-b from-ycs-pink to-ycs-pink/10 rounded-2xl p-8 my-10 mx-auto">
+      <div className="text-white text-lg font-bold pt-16">{title}</div>
+      <div className="text-gray-400">{text}</div>
+    </div>
+  );
+};
 
 export const Home = () => {
   return (
     <div className="">
       <div className="flex flex-col items-center">
-        <h1 className="text-white text-8xl font-extrabold text-center py-10 md:py-3 mt-72 mb-8">Yale Computer Society</h1>
+        <h1 className="text-white text-8xl font-extrabold text-center py-10 md:py-3 mt-60 mb-8">Yale Computer Society</h1>
         <p className="text-gray-400 text-3xl mb-52"> Yale's premier software development and computer science student organization</p>
         <p className="text-gray-400 text-4xl" style={{ letterSpacing: "1em" }}>
           v v v
@@ -41,141 +53,25 @@ export const Home = () => {
         </div>
 
         <div className="w-3/4 mb-8 text-white text-6xl font-medium">Cultivating a passion for programming</div>
-        <div className="w-3/4 text-gray-400 text-3xl mb-20">From development mentorship to hacking nights, we create a community for programmers of all skill levels. </div>
+        <div className="w-3/4 text-gray-400 text-3xl mb-10">From development mentorship to hacking nights, we create a community for programmers of all skill levels. </div>
 
-        <div className="columns-2">
-          <div className="text-center w-96 h-48 bg-gradient-to-b from-ycs-pink to-ycs-pink/10 rounded-2xl">
-            <div className="text-white">dpaoisjdfapoisf</div>
-          </div>
-          <div className=" w-96 h-48 bg-gradient-to-b from-ycs-pink to-ycs-pink/10 rounded-2xl">
-            <div className="text-white">dpaoisjdfapoisf</div>
-          </div>
-          <div className=" w-96 h-48 bg-gradient-to-b from-ycs-pink to-ycs-pink/10 rounded-2xl">
-            <div className="text-white">dpaoisjdfapoisf</div>
-          </div>
-        </div>
+        <div className="columns-2 gap-4"></div>
 
-        {/* <div className="overlap-group">
-          <p className="p">Offer real-world software engineering projects to work on collaboratively.</p>
-          <div className="text-wrapper-7">Development</div>
-        </div>
-        <div className="overlap-2">
-          <p className="text-wrapper-8">Plan social events for students to meet and share their interests.</p>
-          <div className="text-wrapper-9">Social Events</div>
-        </div>
-        <div className="overlap-3">
-          <p className="text-wrapper-10">Provide mentorship for those hoping to learn more about programming.</p>
-          <div className="text-wrapper-11">Mentorship</div>
-        </div>
-
-        <div className="overlap-5">
-          <p className="from-development">
-            From development mentorship to hacking nights, <br />
-            we create a community for programmers of all skill levels.
-          </p>
-          <p className="cultivating-a">
-            Cultivating a passion <br />
-            for programming
-          </p>
-        </div>
-        <p className="whether-it-s-meeting">
-          Whether it’s meeting up with new Yalies for a meal or exploring Yale dorm selections, we’re always thinking of ways to <br />
-          help the student body.
-        </p>
-        <p className="text-wrapper-13">We’re always coming up with new ideas to make things easier.</p>
-        <div className="overlap-6">
-          <div className="group">
-            <p className="text-wrapper-14">The best place to shop for classes at Yale.</p>
-            <div className="text-wrapper-15">PRODUCT</div>
-            <div className="overlap-group-wrapper">
-              <div className="div-wrapper">
-                <div className="text-wrapper-12">Learn more</div>
-              </div>
-            </div>
-            <div className="text-wrapper-16">CourseTable</div>
+        <div className="flex w-3/4 mb-16">
+          <div className="w-1/2 mr-10">
+            <GradientBox title="Development" text="Offer real-world software engineering projects to work on collaboratively." />
+            <GradientBox title="Mentorship" text="Provide mentorship for those hoping to learn more about programming." />
           </div>
-          <div className="overlap-wrapper">
-            <div className="overlap-7">
-              <div className="device-macbook-pro">
-                <div className="overlap-7">
-                  <div className="shadow" />
-                  <div className="top">
-                    <div className="overlap-group-2">
-                      <div className="dark-screen" />
-                      <div className="screen-wrap" />
-                    </div>
-                  </div>
-                  <div className="bottom">
-                    <div className="overlap-8">
-                      <div className="base" />
-                      <img className="bottom-curve" alt="Bottom curve" src="https://generation-sessions.s3.amazonaws.com/946ecc0b964fb4a6ddf8c088ea18b080/img/bottom-curve.svg" />
-                      <img className="groove" alt="Groove" src="https://generation-sessions.s3.amazonaws.com/946ecc0b964fb4a6ddf8c088ea18b080/img/groove.svg" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <img className="screenshot" alt="Screenshot" src="https://generation-sessions.s3.amazonaws.com/946ecc0b964fb4a6ddf8c088ea18b080/img/screenshot-2023-07-30-at-2-13-2.png" />
-            </div>
-          </div>
-        </div>
-        <div className="group-2">
-          <div className="overlap-9">
-            <p className="text-wrapper-17">Then, join the y/cs mentorship to get the wheels turning and jumpstart your programming career at Yale.</p>
-            <div className="explore-button-2">
-              <div className="div-wrapper">
-                <div className="text-wrapper-12">Learn more</div>
-              </div>
-            </div>
-          </div>
-          <div className="text-wrapper-18">PROGRAM</div>
-          <p className="have-an-idea-but-don">
-            Have an idea, but don’t <br />
-            know how to get started?
-          </p>
-        </div>
-        <div className="group-wrapper">
-          <div className="group-3">
-            <p className="text-wrapper-19">Meet Yalies who you’ll vibe with. Trust us, we know.</p>
-            <div className="explore-button-3">
-              <div className="overlap-group-3">
-                <div className="text-wrapper-20">Explore</div>
-              </div>
-            </div>
-            <div className="text-wrapper-21">MealMatch</div>
-          </div>
-        </div>
-        <div className="group-4">
-          <div className="group-5">
-            <p className="text-wrapper-19">Explore Yale dorms to find the best suite for you.</p>
-            <div className="explore-button-3">
-              <div className="overlap-group-3">
-                <div className="text-wrapper-20">Explore</div>
-              </div>
-            </div>
-            <div className="text-wrapper-21">RoomAdvisor</div>
+          <div className="w-1/2 flex flex-col justify-start md:justify-center items-start md:items-end">
+            <GradientBox title="Events" text="Plan social events for students to meet and share their interests." />
           </div>
         </div>
 
-        <div className="group-6">
-          <div className="text-wrapper-30">JOIN THE COMMUNITY</div>
-          <p className="program-with-us-at">
-            Program with us at
-            <br />
-            the Yale Computer Society
-          </p>
-          <div className="group-7">
-            <div className="text-wrapper-31">Github</div>
-            <div className="overlap-group-4">
-              <div className="text-wrapper-32">Instagram</div>
-              <img className="image-6" alt="Image" src="https://generation-sessions.s3.amazonaws.com/946ecc0b964fb4a6ddf8c088ea18b080/img/image-11@2x.png" />
-              <img className="image-6" alt="Image" src="https://generation-sessions.s3.amazonaws.com/946ecc0b964fb4a6ddf8c088ea18b080/img/image-11@2x.png" />
-            </div>
-            <div className="text-wrapper-33">YaleConnect</div>
-            <img className="image-7" alt="Image" src="https://generation-sessions.s3.amazonaws.com/946ecc0b964fb4a6ddf8c088ea18b080/img/image-12@2x.png" />
-            <img className="image-8" alt="Image" src="https://generation-sessions.s3.amazonaws.com/946ecc0b964fb4a6ddf8c088ea18b080/img/image-10@2x.png" />
-          </div>
+        <div className="w-3/4 mb-8 text-white text-5xl font-medium text-left">Have an idea, but don't know how to get started?</div>
+        <div className="w-3/4 text-gray-400 text-left text-3xl font-normal mb-8">Join the y/cs mentorship to get the wheels turning and jumpstart your programming career at Yale. </div>
+        <div className="w-3/4 flex items-center">
+          <div className="w-44 h-12 relative bg-ycs-pink rounded-full text-white text-xl font-bold flex items-center text-center justify-center">Learn More</div>
         </div>
-      <p className="text-wrapper-34">Yale Computer Society © 2023</p> */}
       </div>
     </div>
   );
