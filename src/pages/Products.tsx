ab976e3d5814fc94fa362d0f9c24fce2a9afc680
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-08-08 10:17:19 -0500

    Fix background color change for older pages

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,67 +1,59 @@
-import { Fragment } from "react"
-import { Project, PROJECTS } from "../data"
-import { GeneralProduct } from "./GeneralProduct"
-import { Link } from "react-router-dom"
+import { Fragment } from "react";
+import { Project, PROJECTS } from "../data";
+import { GeneralProduct } from "./GeneralProduct";
+import { Link } from "react-router-dom";
 
 type ProjectCardProps = {
-    project: Project
-}
+  project: Project;
+};
 
-const ProjectCard = ({project}: ProjectCardProps) => (
-    <div className={project.live ? 'project-card' : 'card'} style={{ display: 'flex', flexDirection: 'row' }}>
-        <div style={{ width: '25%' }}>
-            <img src={project.image} loading='lazy' alt={project.name} style={{ maxWidth: '80%', maxHeight: '150px', borderRadius: '15px' }}/>
-        </div>
-        <div style={{ width: '75%' }}>
-            <strong style={{ marginTop: '10px' }}>
-                {project.name}
-            </strong>
-            <p>{project.description}</p>
-        </div>
+const ProjectCard = ({ project }: ProjectCardProps) => (
+  <div className={project.live ? "project-card" : "card"} style={{ display: "flex", flexDirection: "row" }}>
+    <div style={{ width: "25%" }}>
+      <img src={project.image} loading="lazy" alt={project.name} style={{ maxWidth: "80%", maxHeight: "150px", borderRadius: "15px" }} />
     </div>
-)
+    <div style={{ width: "75%" }}>
+      <strong style={{ marginTop: "10px" }}>{project.name}</strong>
+      <p>{project.description}</p>
+    </div>
+  </div>
+);
 
 export const Products = () => {
-
-    return (
-        <div style={{ paddingTop: '10vh' }}> 
-            <h2>Live Products</h2>
-            <div className='product-grid'>
-                {
-                    PROJECTS.filter((project) => project.live).map((project, i) => (
-                        <Fragment key={i}>
-                            {
-                                project.name === "CourseTable" ? 
-                                <Link to="/products/CourseTable" style={{ textDecoration: "none", color: "inherit" }}>
-                                    <ProjectCard project={project}/>
-                                </Link> :
-                                project.link ?
-                                <a href={project.link} target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
-                                    <ProjectCard project={project}/>
-                                </a> :
-                                <ProjectCard project={project}/>
-                            }
-                        </Fragment>
-                    ))
-                }
-            </div>
-            <h2>Products in Production</h2>
-            <div className='product-grid'>
-                {
-                    PROJECTS.filter((project) => !project.live).map((project, i) => (
-                        <Fragment key={i}>
-                            {
-                                project.link ?
-                                <a href={project.link} target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
-                                    <ProjectCard project={project}/>
-                                </a> :
-                                <ProjectCard project={project}/>
-                            }
-                        </Fragment>
-                    ))
-                }
-            </div>
-        </div>
-    )
-
-}
+  return (
+    <div style={{ paddingTop: "10vh", background: "white" }}>
+      <h2>Live Products</h2>
+      <div className="product-grid">
+        {PROJECTS.filter((project) => project.live).map((project, i) => (
+          <Fragment key={i}>
+            {project.name === "CourseTable" ? (
+              <Link to="/products/CourseTable" style={{ textDecoration: "none", color: "inherit" }}>
+                <ProjectCard project={project} />
+              </Link>
+            ) : project.link ? (
+              <a href={project.link} target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
+                <ProjectCard project={project} />
+              </a>
+            ) : (
+              <ProjectCard project={project} />
+            )}
+          </Fragment>
+        ))}
+      </div>
+      <h2>Products in Production</h2>
+      <div className="product-grid">
+        {PROJECTS.filter((project) => !project.live).map((project, i) => (
+          <Fragment key={i}>
+            {project.link ? (
+              <a href={project.link} target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
+                <ProjectCard project={project} />
+              </a>
+            ) : (
+              <ProjectCard project={project} />
+            )}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
