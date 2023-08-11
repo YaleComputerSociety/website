3c2ea43963716ed156a22681b90dcf65e882a950
Kelvin Yip <kelvinyip7@Kelvins-MBP.home>
2023-08-11 15:07:10 -0400

    Adding General Lab component, Adding Ymeets and autodonation pages

---
+++ src/pages/GeneralLab.tsx
@@ -0,0 +1,25 @@
+import { GeneralProductHeader } from "./components/GeneralProductHeader";
+
+interface GeneralLabProps {
+    productName: string;
+    productColor: string;
+    productSlogan: string;  
+}
+
+export const GeneralLab = ({
+    productName,
+    productColor,
+    productSlogan
+}: GeneralLabProps) => {
+
+    return (
+        <GeneralProductHeader
+            productName={productName}
+            productColor={productColor}
+            productSlogan={productSlogan}
+            productLink='https://airtable.com/appqm7yvqSUcLQKV4/shrRxMb5gJOPjl20A'
+            buttonDescription='Get Updates 🛠️'
+        />
+    )
+
+}
