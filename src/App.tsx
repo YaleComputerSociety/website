6dda423e983e72e2a4ffe56232a131e6a3a0b891
Kelvin Yip <kelvinyip7@Kelvins-MBP.home>
2023-08-14 11:50:26 -0400

    Cleaning up Home code

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,36 @@ import './App.css';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Products } from './pages/Products';
 import { Team } from './pages/Team';
 import { CourseTable } from './pages/CourseTable';
 import { Ymeets } from './pages/Ymeets';
 import { AutoDonation } from './pages/AutoDonation';
+import { useEffect } from 'react';
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+}
 
 function App() {
+
+    const location = useLocation();
+
+    useEffect(() => {
+        scrollToTop()
+    }, [location]);
+
+    useEffect(() => {
+        if (window.scrollY !== 0) {
+            scrollToTop()
+        }
+    }, [])
+
     return (
         <div className="App">
             <Header/>
