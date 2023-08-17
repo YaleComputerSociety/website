d98a259a11f22b1f42e7856803198489b71a6f47
Kelvin Yip <kelvinyip7@Kelvins-MBP.lan>
2023-08-17 00:18:21 -0400

    Add Yalies, YaleButteries, MealMatch page, Add generalized Product Bar

--- src/pages/AutoDonation.tsx
+++ src/pages/AutoDonation.tsx
@@ -6,6 +6,7 @@ export const AutoDonation = () => {
             productName="AutoDonation" 
             productColor="#A05AA4" 
             productSlogan="Intuitive, automated donations for New Haven nonprofits." 
+            isWeb
         />
     )
 }
