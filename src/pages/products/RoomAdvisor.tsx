958f6b627612392349e1f0370c3ca6d79cc29ef2
Kelvin Yip <kelvinyip7@Kelvins-MBP.lan>
2023-08-20 01:22:44 -0400

    Add RoomAdvisor page, update sizing of Products

---
+++ src/pages/products/RoomAdvisor.tsx
@@ -0,0 +1,13 @@
+
+import { GeneralLab } from "../GeneralLab"
+
+export const RoomAdvisor = () => {
+    return (
+        <GeneralLab 
+            productName="RoomAdvisor" 
+            productColor="#0053C5" 
+            productSlogan="Explore Yale dorms to find the best suite for you." 
+            isWeb
+        />
+    )
+}
