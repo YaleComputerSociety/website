cd039ab6728bd773d5a7dd22befe87aec44dbdf5
Kelvin Yip <kelvinyip7@Kelvins-MBP.lan>
2023-08-20 01:22:44 -0400

    Add RoomAdvisor page, update sizing of Products

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -5,9 +5,9 @@ interface SectionTitleProps {
 
 export const SectionTitle = ({header, subheader}: SectionTitleProps) => (
 
-    <div style={{paddingTop: "10vh", paddingBottom: "10vh"}}>
+    <div style={{paddingTop: "10vh", paddingBottom: "10vh", width: "100%"}}>
         <h1 style={{fontSize: "65px", fontWeight: "700"}}>{header}</h1>
-        <h4 style={{fontSize: "20px", marginTop: "21px"}}>{subheader}</h4>
+        <h4 style={{fontSize: "20px", fontWeight: "400", marginTop: "21px", marginLeft: "30%", marginRight: "30%", textAlign: "center"}}>{subheader}</h4>
     </div>
 
 )
