f4357bae6199baa6c519ee32c463526cc077808e
Ethan <ethan.mathieu@yale.edu>
2023-08-22 21:33:44 -0400

    modified the home page, specifically gradient blocks and the y/cs and arrow text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,39 @@ import { ProductBar } from "../components/ProductBar";
 interface GradientBoxProps {
   title: string;
   text: string;
+  color: 'pink' | 'blue' | 'green' | 'faded-pink'; // Define the possible colors here
 }
 
-const GradientBox: FC<GradientBoxProps> = ({ title, text }) => {
+const GradientBox: FC<GradientBoxProps> = ({ title, text, color }) => {
+  let gradientColors;
+
+  switch (color) {
+    case 'pink':
+      gradientColors = 'from-ycs-pink to-ycs-pink/10';
+      break;
+    case 'faded-pink':
+      gradientColors = 'from-ycs-faded-pink to-ycs-faded-pink/10';
+      break;
+    case 'blue':
+      gradientColors = 'from-ycs-blue to-ycs-blue/10';
+      break;
+    case 'green':
+      gradientColors = 'from-ycs-green to-ycs-green/10';
+      break;
+    default:
+      gradientColors = 'from-ycs-pink to-ycs-pink/10';
+  }
+
   return (
-    <div className="w-full h-56 text-left bg-gradient-to-b from-ycs-pink to-ycs-pink/10 rounded-2xl p-8 my-10 mx-auto">
-      <div className="text-white text-lg font-bold pt-16">{title}</div>
-      <div className="text-gray-400">{text}</div>
+    <div className={`w-full h-full text-left bg-gradient-to-b ${gradientColors} rounded-2xl p-8 my-10 mx-auto relative`}>
+      <div className="p-6">
+        <div className="text-white text-lg font-bold">
+          <div className="absolute bottom-0 left-0 right-0 p-6">
+            <p className="text-white">{title}</p>
+            <p className="text-gray-400 overflow-hidden whitespace-wrap">{text}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -20,30 +46,62 @@ const GradientBox: FC<GradientBoxProps> = ({ title, text }) => {
 export const Home = () => {
   return (
     <div className="">
-      <div className="flex flex-col items-center">
-        <h1 className="text-white text-8xl font-extrabold text-center py-10 md:py-3 mt-60 mb-8">Yale Computer Society</h1>
-        <p className="text-gray-400 text-3xl mb-52"> Yale's premier software development and computer science student organization</p>
-        <p className="text-gray-400 text-4xl" style={{ letterSpacing: "1em" }}>
-          v v v
-        </p>
-        <ProductBar productBarHeader="Check out our work"/>
+      <div className="flex flex-col text-shadow shadow-black  items-center">
+        <h1 className="text-white text-big font-extrabold text-shadow-xl shadow-black text-center py-10 md:py-3 mt-60 mb-8">y/cs</h1>
+        <p className="text-white w-1/2 text-3xl mb-6">We are the Yale Computer Society.</p>
+        <p className="text-gray-400 w-1/2 text-2xl mb-6">Yale's premier software development, computer science and tech entrepreneurship student organization.</p>
+     
+      <div
+        style={{    transform: "translateY(200%)",
+        margin: "auto",
+        width: "5%",
+        padding: "10px",
+        position: "relative",
+        bottom: "0",
+        animation: "bounce 4s",
+        animationDirection: "alternate",
+        animationIterationCount: "infinite",
+        color: "white"}}
+      >
+        <div
+          style={{
+            border: "solid white",
+            borderWidth: "0 10px 10px 0",
+            padding: "20px",
+            transform: "rotate(45deg)",
+            WebkitTransform: "rotate(45deg)", // For Webkit browsers
+            justifyContent: "center",
+            alignItems: "center",
+            filter: "drop-shadow(4px 4px 2px black)"
+          }}
+        >
+          {/* Your content */}
+        </div>
+      </div>
+
+      
+        <ProductBar productBarHeader="Our Development Teams"/>
         <div className="w-3/4 mb-8 text-white text-6xl font-medium">Cultivating a passion for programming</div>
         <div className="w-3/4 text-gray-400 text-3xl mb-10">From development mentorship to hacking nights, we create a community for programmers of all skill levels. </div>
 
         <div className="columns-2 gap-4"></div>
 
-        <div className="flex w-3/4 mb-16">
-          <div className="w-1/2 mr-10">
-            <GradientBox title="Development" text="Offer real-world software engineering projects to work on collaboratively." />
-            <GradientBox title="Mentorship" text="Provide mentorship for those hoping to learn more about programming." />
-          </div>
-          <div className="w-1/2 flex flex-col justify-start md:justify-center items-start md:items-end">
-            <GradientBox title="Events" text="Plan social events for students to meet and share their interests." />
+          <div className="flex w-3/4 mb-12 h-3/4">
+            <div className="w-1/2 mr-10 flex flex-grow">
+              <GradientBox title="Development" color="blue" text="Become a member of one of our software teams and help build apps that benefit the Yale and New Haven community." />
+            </div>
+            <div className="w-1/2 flex flex-col justify-between"> 
+            <div className="mb-8">
+              <GradientBox title="Mentorship" color="pink" text="New to software engineering? Learn from mentors who have been there before with the y/cs New Developers Program " />
+            </div>
+            <div>
+              <GradientBox title="Events" color="green" text="Help plan social events for students to meet and share their interests, as well as bring notable industry figures to Yale's campus." />
+            </div>
           </div>
         </div>
-
-        <div className="w-3/4 mb-8 text-white text-5xl font-medium text-left">Have an idea, but don't know how to get started?</div>
-        <div className="w-3/4 text-gray-400 text-left text-3xl font-normal mb-8">Join the y/cs mentorship to get the wheels turning and jumpstart your programming career at Yale. </div>
+    
+        <div className="w-3/4 mt-20 mb-8 text-white text-5xl font-medium text-left">Ready to join the best computer science club at Yale?</div>
+        <div className="w-3/4 text-gray-400 text-left text-3xl font-normal mb-8">Follow the link to join our slack channel and earn you general membership, and watch out for our software team recruiting cycles!</div>
         <div className="w-3/4 flex items-center">
           <div className="w-44 h-12 relative bg-ycs-pink rounded-full text-white text-xl font-bold flex items-center text-center justify-center">Learn More</div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+        xl : '0 16px 32px var(--tw-shadow-color)'
+      },
       colors: {
         "ycs-black": "#1F232C",
         "ycs-pink": "#FF555B",
+        "ycs-faded-pink" : "F26891",
+        "ycs-blue" : "#0071BC",
+        "ycs-green" : "#39A393",
+      },
+      fontSize: {
+        'big': '200px',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 };
