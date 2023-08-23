ebb444f80bb1c3693f8eb159da1570f573b16f10
Ethan <ethan.mathieu@yale.edu>
2023-08-22 21:33:44 -0400

    modified the home page, specifically gradient blocks and the y/cs and arrow text

--- src/components/ProductBar.tsx
+++ src/components/ProductBar.tsx
@@ -8,8 +8,8 @@ export const ProductBar = ({productBarHeader}: ProductBarProps) => {
 
     return (
         <div className="py-28 flex flex-col items-center w-full">
-            <div className="text-center text-white text-3xl font-normal pb-12">{productBarHeader}</div>
-            <div className="w-3/4 h-28 bg-gray-400 rounded-full flex items-center justify-evenly">
+            <div className="text-center bg-ycs-blue text-white text-3xl rounded-md font-normal p-4 mb-12 ">{productBarHeader}</div>
+            <div className="w-3/4 h-28 bg-gray-400 shadow-lg shadow-black rounded-full flex items-center justify-evenly">
             {
                 PROJECTS.map((project, i) => (
                     <img className="w-20 h-20 rounded-lg" key={i} src={project.image} alt={project.name} />
