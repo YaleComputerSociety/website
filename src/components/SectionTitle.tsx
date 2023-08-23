cc72d44178e938573dcc5c7d05727580dc18ff44
Kelvin Yip <kelvinyip7@Kelvins-MBP.lan>
2023-08-22 22:16:51 -0400

    mobile-optimizations for product page

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -7,7 +7,7 @@ export const SectionTitle = ({header, subheader}: SectionTitleProps) => (
 
     <div style={{paddingTop: "10vh", paddingBottom: "10vh", width: "100%"}}>
         <h1 style={{fontSize: "65px", fontWeight: "700"}}>{header}</h1>
-        <h4 style={{fontSize: "20px", fontWeight: "400", marginTop: "21px", marginLeft: "30%", marginRight: "30%", textAlign: "center"}}>{subheader}</h4>
+        <h4 style={{fontSize: "20px", fontWeight: "400", marginTop: "21px", marginLeft: "20%", marginRight: "20%", textAlign: "center"}}>{subheader}</h4>
     </div>
 
 )
