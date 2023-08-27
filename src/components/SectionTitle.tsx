18b676f56b400ce3f93ffeaf6c48bde3e958d9d9
Ethan <ethan.mathieu@yale.edu>
2023-08-27 00:23:16 -0400

    events blog page + cal

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -21,10 +21,11 @@ export const SectionTitle = ({ title: header, subtitle: subheader }: SectionTitl
 
 export const SectionTitle = ({header, subheader}: SectionTitleProps) => (
 
-    <div style={{paddingTop: "10vh", paddingBottom: "10vh", width: "100%"}}>
-        <h1 style={{fontSize: "65px", fontWeight: "700"}}>{header}</h1>
-        <h4 style={{fontSize: "20px", fontWeight: "400", marginTop: "21px", marginLeft: "20%", marginRight: "20%", textAlign: "center"}}>{subheader}</h4>
-    </div>
+<div style={{ paddingTop: "10vh", paddingBottom: "10vh", width: "50%", margin: "0 auto", textAlign: "center" }}>
+    <h1 className="bg-ycs-pink p-4 rounded-md" style={{ fontSize: "65px", fontWeight: "700" }}>{header}</h1>
+    <h4 style={{ fontSize: "20px", fontWeight: "400", marginTop: "21px" }}>{subheader}</h4>
+</div>
+
 
 )
 >>>>>>> 359653e (New pages, sticky header)
