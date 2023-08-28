6f8da41809ed8f5ea337fa3920ff437fd76edac2
Ean244 <ianlim224@gmail.com>
2023-08-27 23:41:29 -0400

    Finished implementing about page Created new InfoCard component for displaying statistics on About page Added about page content to data.ts

---
+++ src/components/Company.tsx
@@ -0,0 +1,12 @@
+interface SectionTitleProps {
+    img: string;
+    name: string;
+}
+
+export const SectionTitle = ({img, name}: SectionTitleProps) => (
+
+    <div className="flex flex-row justify-center">
+
+    </div>
+
+)
