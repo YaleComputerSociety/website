55443c2b3c97580e4810dafa487e9f64e4d623cf
Ethan Mathieu <ethanmathieu@Ethans-MacBook-Pro-2.local>
2023-08-28 23:20:07 -0400

    fixed partner and stats

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,18 +8,20 @@ export const Header = () => {
         &lt;y/cs&gt;
       </Link>
       <div className="pagesList">
-        <Link className="header-link" to="/team">
-          Team
-        </Link>
+      <Link className="header-link" to="/about">
+              About
+          </Link>
+       
         <Link className="header-link" to="/products">
           Development
         </Link>
         <Link className="header-link" to="/events">
           Events
         </Link>
-          <Link className="header-link" to="/about">
-              About
-          </Link>
+        <Link className="header-link" to="/team">
+          Team
+        </Link>
+        
         <Link className="header-link" target="_blank" to="https://airtable.com/appuVd3lWwTikuv40/shrLYVYcHUDQa8T3h">
           Feedback
         </Link>
