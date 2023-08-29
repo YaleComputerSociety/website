89a8f0a50398c207ef433e0a4eaaf6c03dd2f3f2
Ethan Mathieu <ethanmathieu@Ethans-MacBook-Pro-2.local>
2023-08-28 23:20:07 -0400

    fixed partner and stats (mod rts49)

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <Link className="logo" to="/">
+        &lt;y/cs&gt;
+      </Link>
+      <div className="pagesList">
+      <Link className="header-link" to="/about">
+              About
+          </Link>
+       
+        <Link className="header-link" to="/products">
+          Development
+        </Link>
+        <Link className="header-link" to="/events">
+          Events
+        </Link>
+        <Link className="header-link" to="/team">
+          Team
+        </Link>
+        
+        <Link className="header-link" target="_blank" to="https://airtable.com/appuVd3lWwTikuv40/shrLYVYcHUDQa8T3h">
+          Feedback
+        </Link>
+      </div>
+    </div>
+  );
+};
