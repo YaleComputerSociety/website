d0ee49777c4a72e272f869fe87ac8c7ae548156d
Ethan M <ethan.mathieu@yale.edu>
2024-02-17 13:02:13 -0500

    addison wants this push

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Footer } from './components/Footer';
-import { Header } from './components/Header';
+import Header from './components/Header';
 import { Home } from './pages/Home';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { Products } from './pages/Products';
