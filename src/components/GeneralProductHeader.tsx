dcc72fe552e1ed3d9443bc2bf86d544642d54eee
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-20 21:20:27 -0500

    Delete all remaining ds stores, merge the two component folders, create styles folder

--- src/components/GeneralProductHeader.tsx
+++ src/components/GeneralProductHeader.tsx
@@ -1,4 +1,4 @@
-import '../GeneralProduct.css'
+import '../styles/GeneralProduct.css'
 
 interface GeneralProductHeaderProps {
     productName: string;
@@ -24,7 +24,7 @@ export const GeneralProductHeader = ({
                 <div className='text-container' style={{ width: "60%" }}>
                     <div style={{ display: "flex", alignItems: "center", width: "100%" }}>
                         <div style={{ fontWeight: 700, color: "white", fontSize: "44px", float: "left", display: "inline-block" }}>{productName}</div> 
-                        <img className='w-20 h-20 rounded-lg' src={require(`../../assets/products/${productName}/productlogo.png`)} alt={productName} style={{ marginLeft: "1rem", display: "inline-block" }} />
+                        <img className='w-20 h-20 rounded-lg' src={require(`../assets/products/${productName}/productlogo.png`)} alt={productName} style={{ marginLeft: "1rem", display: "inline-block" }} />
                     </div>
                     <div style={{ fontWeight: 400, color: "white", fontSize: "24px", textAlign: "left" }}>{productSlogan}</div>
                     <div className='visit-button' style={{ backgroundColor: productColor }} onClick={() => window.open(productLink, "_blank")}>
@@ -35,10 +35,10 @@ export const GeneralProductHeader = ({
             <div className='section-image-container'>
                 {
                     isWeb ? 
-                    <img src={require(`../../assets/products/${productName}/headerimage.png`)} style={{ width: "85%" }} alt="Product Main Screenshot"/> :
+                    <img src={require(`../assets/products/${productName}/headerimage.png`)} style={{ width: "85%" }} alt="Product Main Screenshot"/> :
                     <>
-                        <img src={require(`../../assets/products/${productName}/headerimage1.png`)} style={{ width: "35%", marginRight: "5%" }} alt="Product Main Mobile Screenshot 1"/>
-                        <img src={require(`../../assets/products/${productName}/headerimage2.png`)} style={{ width: "35%", marginLeft: "5%" }} alt="Product Main Mobile Screenshot 2"/>
+                        <img src={require(`../assets/products/${productName}/headerimage1.png`)} style={{ width: "35%", marginRight: "5%" }} alt="Product Main Mobile Screenshot 1"/>
+                        <img src={require(`../assets/products/${productName}/headerimage2.png`)} style={{ width: "35%", marginLeft: "5%" }} alt="Product Main Mobile Screenshot 2"/>
                     </>
                 }
             </div>
