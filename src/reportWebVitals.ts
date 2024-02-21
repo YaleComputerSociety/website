be6c778bd578c9d4a5307f2583964718cfac4ed8
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-20 20:33:30 -0500

    Remove several unused files

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,5 +1,9 @@
 import { ReportHandler } from 'web-vitals';
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
