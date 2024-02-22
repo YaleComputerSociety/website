3daf704b4bfc86ee29a3d9b99a392531b46599ae
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-21 23:12:24 -0500

    Add in eslintrc file and vscode settings from nextjs project version (forgot to copy over previously; configure build to generate a static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
