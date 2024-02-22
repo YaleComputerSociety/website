90b579f7ea14bf03516f0449d90253b1de84ebd8
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-21 23:01:02 -0500

    Rewrite the entire codebase in nextjs with stricter eslint and typescript settings

--- src/data.ts
+++ src/data.ts
@@ -1,40 +1,47 @@
 // Product imports
+
+import { StaticImageData } from "next/image";
+
 import coursetable from "./assets/products/logos/coursetable.png";
 import yalies from "./assets/products/logos/yalies.png";
 import ymeets from "./assets/products/logos/ymeets.png";
 import yalebutteries from "./assets/products/logos/yalebutteries.jpg";
-import mealmatch from "./assets/products/logos/MealMatch.png";
 import roomadvisor from "./assets/products/logos/roomadvisor.png";
 import autodonation from "./assets/products/logos/autodonation.svg";
-
 // Media imports
 import email from "./assets/email2.png";
 import facebook from "./assets/facebook.svg";
 import instagram from "./assets/instagram.svg";
-
 // People imports
 import kelvin from "./assets/people/kelvin.jpg";
 import bill from "./assets/people/bill.jpg";
-import erik from "./assets/people/erik.jpg";
 import katherine from "./assets/people/katherine.jpg";
 import lauren from "./assets/people/lauren.jpg";
-import kenny from "./assets/people/kenny.jpg";
 import addison from "./assets/people/addison.jpg";
 import nabil from "./assets/people/nabil.jpg";
-import aidan from "./assets/people/aidan.jpg";
 import ethan from "./assets/people/ethan.jpg";
-import lucas from "./assets/people/lucas.jpg";
+import amina from "./assets/people/amina.png";
+import anna from "./assets/people/anna.jpeg";
+import daniel from "./assets/people/daniel.jpeg";
+import betti from "./assets/people/betti.jpeg";
+import lorenss from "./assets/people/lorenss.jpeg";
+import kai from "./assets/people/kai.jpeg";
+import jessica from "./assets/people/jessica.jpeg";
+import fatima from "./assets/people/fatima.jpeg";
+import tyler from "./assets/people/tyler.jpeg";
+import alex from "./assets/people/alex.jpeg";
+import sida from "./assets/people/sida.jpeg";
+import chase from "./assets/people/chase.jpeg";
+import nicholas from "./assets/people/nicholas.jpeg";
+import neil from "./assets/people/neil.jpeg";
+import ericyoon from "./assets/people/ericyoon.jpeg";
 import defaultPerson from "./assets/people.jpeg";
-
 // partnerships import
-import meta from "./assets/partnerships/meta.png"
-import bloomberg from "./assets/partnerships/bloomberg.jpeg"
-import wgics from "./assets/partnerships/wgics.png"
-import yes from "./assets/partnerships/yes.png"
-import yhack from "./assets/partnerships/yhack.png"
-import exp from "constants";
-import janestreet from "./assets/partnerships/JS Logo 2023.png"
-import tsai from "./assets/partnerships/tsaicity.png"
+import meta from "./assets/partnerships/meta.png";
+import bloomberg from "./assets/partnerships/bloomberg.jpeg";
+import wgics from "./assets/partnerships/wgics.png";
+import janestreet from "./assets/partnerships/JS Logo 2023.png";
+import tsai from "./assets/partnerships/tsaicity.png";
 
 export const BOARD = [
   {
@@ -54,28 +61,33 @@ export const BOARD = [
   },
   {
     name: "Amina Sakho",
-    image: defaultPerson,
+    image: amina,
     role: ["Vice Director of Development"],
   },
   {
-    name: "Kai Zhang",
-    image: defaultPerson,
-    role: ["Director of Design"],
+    name: "Betti Tang",
+    image: betti,
+    role: ["Co-Director of Events"],
   },
   {
-    name: "Emillie Ma",
-    image: defaultPerson,
-    role: ["Director of Events"],
+    name: "Daniel Wang",
+    image: daniel,
+    role: ["Co-Director of Events"],
   },
   {
     name: "Jessica Le",
-    image: defaultPerson,
-    role: ["Director of Outreach and Recruitment"],
+    image: jessica,
+    role: ["Co-Director of Outreach & Recruitment"],
   },
   {
     name: "Fatima Kamara",
-    image: defaultPerson,
-    role: ["Vice Director of Outreach and Recruitment"],
+    image: fatima,
+    role: ["Co-Director of Outreach & Recruitment"],
+  },
+  {
+    name: "Kai Zhang",
+    image: kai,
+    role: ["Director of Design"],
   },
   {
     name: "Lauren Delwiche",
@@ -89,7 +101,7 @@ export const BOARD = [
   },
   {
     name: "Tyler Schroder",
-    image: defaultPerson,
+    image: tyler,
     role: ["Communications"],
   },
   {
@@ -100,28 +112,38 @@ export const BOARD = [
   // product leads
   {
     name: "Alex Schapiro",
-    image: defaultPerson,
+    image: alex,
     role: ["CourseTable"],
   },
   {
-    name: "Lucas Huang",
-    image: lucas,
+    name: "Sida Chen",
+    image: sida,
     role: ["CourseTable"],
   },
   {
-    name: "Erik Boesen",
-    image: erik,
+    name: "Eric Yoon",
+    image: ericyoon,
     role: ["Yalies"],
   },
   {
-    name: "Aidan Palmer",
-    image: aidan,
-    role: ["YaleButteries"],
+    name: "Nicholas Ribeiro",
+    image: nicholas,
+    role: ["ymeets"],
   },
   {
-    name: "Nicholas Ribeiro",
+    name: "Chase Reynders",
+    image: chase,
+    role: ["Yale Butteries"],
+  },
+  {
+    name: "Ian Lim",
     image: defaultPerson,
-    role: ["ymeets"],
+    role: ["Yale Clubs"],
+  },
+  {
+    name: "Lorenss Martinsons",
+    image: lorenss,
+    role: ["MajorAudit"],
   },
   {
     name: "Bill Qian",
@@ -129,45 +151,45 @@ export const BOARD = [
     role: ["RoomAdvisor"],
   },
   {
-    name: "Kenny Tung",
-    image: kenny,
-    role: ["RoomAdvisor"],
+    name: "Neil Shah",
+    image: neil,
+    role: ["AutoDonation"],
   },
   {
-    name: "Katherine He",
-    image: katherine,
-    role: ["AutoDonation"],
+    name: "Anna Xu",
+    image: anna,
+    role: ["Yale IMs"],
   },
 ];
 
 export type Project = {
   name: string;
-  image: string;
+  image: StaticImageData;
   link: string;
   live: boolean;
   description: string;
 };
 
-
 export const PROJECTS: Project[] = [
   {
     name: "CourseTable",
     image: coursetable,
-    link: "/products/CourseTable",
-    description: "A student-built courses browser used by nearly everyone at Yale.",
+    link: "/products/coursetable",
+    description:
+      "A student-built courses browser used by nearly everyone at Yale.",
     live: true,
   },
   {
     name: "Yalies",
     image: yalies,
-    link: "/products/Yalies",
+    link: "/products/yalies",
     description: "A directory that provides data on the Yale student body.",
     live: true,
   },
   {
     name: "YaleButteries",
     image: yalebutteries,
-    link: "/products/YaleButteries",
+    link: "/products/yalebutteries",
     description: "An app to transform the buttery experience at Yale.",
     live: false,
   },
@@ -175,22 +197,25 @@ export const PROJECTS: Project[] = [
   {
     name: "ymeets",
     image: ymeets,
-    link: "/products/Ymeets",
-    description: "A web application aimed to make it easier for Yalies to find a time to meet.",
+    link: "/products/ymeets",
+    description:
+      "A web application aimed to make it easier for Yalies to find a time to meet.",
     live: false,
   },
   {
     name: "RoomAdvisor",
     image: roomadvisor,
-    link: "/products/RoomAdvisor",
-    description: "A website where Yale students can browse, rate, and review college rooms.",
+    link: "/products/roomadvisor",
+    description:
+      "A website where Yale students can browse, rate, and review college rooms.",
     live: false,
   },
   {
     name: "AutoDonation",
     image: autodonation,
-    link: "/products/AutoDonation",
-    description: "An intuitive platform that helps nonprofits automate, collect, and track donations.",
+    link: "/products/autodonation",
+    description:
+      "An intuitive platform that helps nonprofits automate, collect, and track donations.",
     live: false,
   },
 ];
@@ -225,59 +250,62 @@ export const MEDIA = [
 export const STORIES = [
   {
     data: "2012",
-    description: "In 2012, then Yale students Peter Xu and Harry Yu created CourseTable – Yale’s first product built " +
-        "by students, for students."
+    description:
+      "In 2012, then Yale students Peter Xu and Harry Yu created CourseTable – Yale’s first product built " +
+      "by students, for students.",
   },
   {
     data: "2016",
-    description: "In 2016, the Yale Computer Society was created as a space for CS enthusiasts to learn and share " +
-        "tech learnings together."
+    description:
+      "In 2016, the Yale Computer Society was created as a space for CS enthusiasts to learn and share " +
+      "tech learnings together.",
   },
   {
     data: "2019",
-    description: "In 2019, the Yale Computer Society combined product and community to begin developing a suite of" +
-        " products for Yale students."
-  }
+    description:
+      "In 2019, the Yale Computer Society combined product and community to begin developing a suite of" +
+      " products for Yale students.",
+  },
 ];
 
 export const TODAY_STATISTICS = [
   {
     data: "120+",
-    description: "members"
+    description: "members",
   },
   {
     data: "20000+",
-    description: "users"
+    description: "users",
   },
   {
     data: "6",
-    description: "software products"
-  }
-]
+    description: "software products",
+  },
+];
 
 export const PARTNERSHIPS = [
   {
     name: "Meta",
-    image: meta
+    image: meta,
   },
 
   {
-    name : "Jane Street",
-    image  : janestreet
+    name: "Jane Street",
+    image: janestreet,
   },
 
   {
     name: "Bloomberg",
-    image: bloomberg
+    image: bloomberg,
   },
 
   {
-    name : "Tsai City \n",
-    image : tsai,
+    name: "Tsai City \n",
+    image: tsai,
   },
 
   {
     name: "Women and Gender Minorities in CS",
-    image: wgics
+    image: wgics,
   },
-]
+];
