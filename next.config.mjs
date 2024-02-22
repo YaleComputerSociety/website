817ad81c8d1e8c9a798011cc67ea411549605eb8
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-21 23:12:24 -0500

    Add in eslintrc file and vscode settings from nextjs project version (forgot to copy over previously; configure build to generate a static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 async function loadConfig() {
   const withBundleAnalyzer = (await import("@next/bundle-analyzer")).default({
@@ -17,6 +18,12 @@ async function loadConfig() {
 export default loadConfig();
 =======
 const nextConfig = {};
+=======
+const nextConfig = {
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
+>>>>>>> 3daf704 (Add in eslintrc file and vscode settings from nextjs project version (forgot to copy over previously; configure build to generate a static site)
 
 export default nextConfig;
 >>>>>>> d4869af (Rewrite the entire codebase in nextjs with stricter eslint and typescript settings)
