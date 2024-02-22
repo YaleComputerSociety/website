3d9f64ec50bb3e8047245b12c30c7bb07087daba
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-22 12:14:06 -0500

    Update package.json scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 async function loadConfig() {
   const withBundleAnalyzer = (await import("@next/bundle-analyzer")).default({
@@ -24,6 +25,9 @@ const nextConfig = {
   reactStrictMode: true,
 };
 >>>>>>> 3daf704 (Add in eslintrc file and vscode settings from nextjs project version (forgot to copy over previously; configure build to generate a static site)
+=======
+const nextConfig = {};
+>>>>>>> 1a5d1b6 (Update package.json scripts)
 
 export default nextConfig;
 >>>>>>> d4869af (Rewrite the entire codebase in nextjs with stricter eslint and typescript settings)
