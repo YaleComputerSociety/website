8dc16be04a7a244a2953ef9168cafb72289eb283
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-23 00:07:09 -0500

    Adjust navbar mobile menu 'x' button and refactor into separate component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,11 @@
 
 import * as React from "react";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
-const navigationItems = [
+import { NavbarMobileMenu } from "./NavbarMobileMenu";
+
+export const navigationItems = [
   { name: "About", link: "/about" },
   { name: "Products", link: "/products" },
   { name: "Events", link: "/events" },
@@ -15,71 +17,13 @@ const navigationItems = [
   },
 ];
 
-const MobileMenu = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement | null>(null);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
-  // functionality for closing the mobile menu
-  const handleClickOutside = (event: MouseEvent) => {
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-      setIsMobileMenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, []);
-
-  return (
-    <div>
-      <button onClick={toggleMobileMenu} className="text-white text-3xl pr-2">
-        <div className="space-y-2">
-          <span className="block w-8 h-0.5 bg-white"></span>
-          <span className="block w-8 h-0.5 bg-white"></span>
-          <span className="block w-8 h-0.5 bg-white"></span>
-        </div>
-      </button>
-      <div
-        ref={menuRef}
-        className={`fixed top-0 right-0 h-full bg-ycs-pink shadow-md transition-transform w-3/4 rounded-bl-[5rem] rounded-tl-[5rem] flex flex-row justify-between ${
-          isMobileMenuOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <button
-          onClick={toggleMobileMenu}
-          className="py-2 px-7 text-6xl order-last self-start font-thin"
-        >
-          ×
-        </button>
-        <ul className="flex flex-col text-3xl px-14 py-28 gap-8">
-          {navigationItems.map((item, index) => (
-            <Link
-              href={item.link}
-              onClick={toggleMobileMenu}
-              key={index}
-              target={!item.link.startsWith("/") ? "_blank" : undefined}
-            >
-              {item.name}
-            </Link>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
 function Navbar() {
   const [isMobile, setIsMobile] = useState(false);
 
   // Check screen width to toggle mobile view
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 600) {
+      if (window.innerWidth < 700) {
         setIsMobile(true);
       } else {
         setIsMobile(false);
@@ -102,7 +46,7 @@ function Navbar() {
         </Link>
       </div>
       {isMobile ? (
-        <MobileMenu />
+        <NavbarMobileMenu />
       ) : (
         <ul className="flex gap-12 text-lg">
           {navigationItems.map((item, index) => (
---
+++ src/components/NavbarMobileMenu.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+
+import { navigationItems } from "./Navbar";
+
+export const NavbarMobileMenu = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // functionality for closing the mobile menu
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsMobileMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  return (
+    <div>
+      <button onClick={toggleMobileMenu} className="text-white text-3xl pr-2">
+        <div className="space-y-2">
+          <span className="block w-8 h-0.5 bg-white"></span>
+          <span className="block w-8 h-0.5 bg-white"></span>
+          <span className="block w-8 h-0.5 bg-white"></span>
+        </div>
+      </button>
+      <div
+        ref={menuRef}
+        className={`fixed top-0 right-0 h-full bg-ycs-pink shadow-md transition-transform w-3/4 rounded-bl-[5rem] rounded-tl-[5rem] flex flex-col ${
+          isMobileMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <button
+          onClick={toggleMobileMenu}
+          className="py-2 px-6 text-6xl font-thin self-end"
+        >
+          ×
+        </button>
+        <ul className="flex flex-col text-3xl p-14 gap-8">
+          {navigationItems.map((item, index) => (
+            <Link
+              href={item.link}
+              onClick={toggleMobileMenu}
+              key={index}
+              target={!item.link.startsWith("/") ? "_blank" : undefined}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
