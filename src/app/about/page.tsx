e0e391bdda3f0a9b41f663f65bbdd5b0f9218579
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-24 17:29:29 -0500

    Adjust section title to be fully responsive'

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,16 +10,9 @@ import { Sponsors } from "@components/Sponsors";
 const About = () => {
   return (
     <div className="flex justify-center items-center flex-col pt-40 pr-40 pl-40 text-white">
-      <SectionTitle
-        header="About Us"
-        subheader="The community for designers and engineers @ Yale."
-      />
+      <SectionTitle title="About Us" subtitle="The community for designers and engineers @ Yale." />
 
-      <Image
-        src={teamPhoto}
-        style={{ width: "auto", height: 344 }}
-        alt="YCS Team"
-      />
+      <Image src={teamPhoto} style={{ width: "auto", height: 344 }} alt="YCS Team" />
 
       <div
         style={{
@@ -31,27 +24,21 @@ const About = () => {
         }}
       >
         <text className="text-2xl">
-          As the place for CS at Yale, we&apos;re a team of computer science
-          enthusiasts looking to revolutionize the campus and make
-          students&apos; lives easier through the use of technology. As a cohort
-          of product designers, software engineers, and computing ambassadors,
-          we develop high-quality, open-source products, host speaker events
-          with technologists and industry professionals, and break barriers for
-          students to gain real-world software engineering experience.
+          As the place for CS at Yale, we&apos;re a team of computer science enthusiasts looking to
+          revolutionize the campus and make students&apos; lives easier through the use of
+          technology. As a cohort of product designers, software engineers, and computing
+          ambassadors, we develop high-quality, open-source products, host speaker events with
+          technologists and industry professionals, and break barriers for students to gain
+          real-world software engineering experience.
         </text>
       </div>
 
       <SmallSectionTitle
         header={"Our Story"}
-        subheader={
-          "The founding and growth of y/cs can be attributed to three crucial years."
-        }
+        subheader={"The founding and growth of y/cs can be attributed to three crucial years."}
       />
 
-      <div
-        className="flex flex-row pt-10 gap-4"
-        style={{ marginBottom: "5vh" }}
-      >
+      <div className="flex flex-row pt-10 gap-4" style={{ marginBottom: "5vh" }}>
         {STORIES.map((story, i) => (
           <InfoCard data={story.data} description={story.description} key={i} />
         ))}
@@ -59,10 +46,7 @@ const About = () => {
 
       <SmallSectionTitle header={"Today"} subheader={"By the Numbers"} />
 
-      <div
-        className="flex flex-row pt-10 gap-4"
-        style={{ marginBottom: "10vh" }}
-      >
+      <div className="flex flex-row pt-10 gap-4" style={{ marginBottom: "10vh" }}>
         {TODAY_STATISTICS.map((story, i) => (
           <InfoCard data={story.data} description={story.description} key={i} />
         ))}
