b642776b1078d4975de7e90c81e5755da88d53e3
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-24 12:22:12 -0500

    Work on adjusting section titles to be responsive

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -4,16 +4,8 @@ interface SectionTitleProps {
 }
 
 export const SectionTitle = ({ header, subheader }: SectionTitleProps) => (
-  <div
-    style={{
-      paddingTop: "10vh",
-      paddingBottom: "10vh",
-      width: "50%",
-      margin: "0 auto",
-      textAlign: "center",
-    }}
-  >
-    <h1 className="bg-ycs-pink p-4 rounded-md" style={{ fontSize: "65px", fontWeight: "700" }}>
+  <div className="text-center py-10 m-auto">
+    <h1 className="bg-ycs-pink p-4 xl:px-8 rounded-md text-2xl md:text-5xl xl:text-7xl font-bold">
       {header}
     </h1>
     <h4 style={{ fontSize: "20px", fontWeight: "400", marginTop: "21px" }}>{subheader}</h4>
