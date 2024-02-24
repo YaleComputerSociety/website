e57233254ac8e0e49ab0d5ab632efab2de3bf41b
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-24 10:40:26 -0500

    Add ian photo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import "@styles/globals.css";
+// import localFont from "next/font/local";
+
 import Navbar from "@components/Navbar";
 import { Footer } from "@components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
+// const mona = localFont({ src: "../fonts/Mona-Sans.woff2" });
 
 export const metadata: Metadata = {
   title: "Yale Computer Society",
