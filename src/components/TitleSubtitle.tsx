f36abc882acaf231a723299b49aa32bdf40601a0
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-26 17:06:36 -0500

    Fix the rest of the bottom of the home page to be responsive. The whole page works on mobile now

--- src/components/TitleSubtitle.tsx
+++ src/components/TitleSubtitle.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const TitleSubtitle: React.FC<Props> = ({ title, subtitle, className }: Props) => {
   return (
     <div className="w-3/4 mt-40 flex flex-col items-center mx-auto">
-      <div className={`text-white lg:text-6xl text-4xl font-bold text-center ${className}`}>
+      <div className={`text-white lg:text-6xl text-3xl font-bold text-center ${className}`}>
         {title}
       </div>
       <div className=" text-gray-400 lg:text-2xl text-lg mt-5 text-center">{subtitle}</div>
