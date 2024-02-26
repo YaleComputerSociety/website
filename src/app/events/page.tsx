b94590b67885fb6a274632c584769b392e3af749
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-26 17:21:12 -0500

    Reorganize events page and make responsive

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,6 +1,12 @@
-const GoogleCalendar = () => {
+import { SectionTitle } from "@components/SectionTitle";
+
+interface Props {
+  className?: string;
+}
+
+const GoogleCalendar: React.FC<Props> = ({ className }: Props) => {
   return (
-    <div className="p-3 bg-ycs-blue border-4 border-solid mr-4 ml-4 border-ycs-blue ">
+    <div className={`border-[0.75rem] rounded-xl border-solid border-ycs-pink ${className}`}>
       <iframe
         src="https://calendar.google.com/calendar/embed?src=t6isudodqcpb2uepck99jmjt3o%40group.calendar.google.com&ctz=America%2FNew_York"
         style={{ border: "0", width: "100%", height: "600px" }}
@@ -12,19 +18,13 @@ const GoogleCalendar = () => {
 
 const Events = () => {
   return (
-    <div className="mt-36 mx-12">
-      <div className="grid grid-cols-2 gap-4">
-        <div className="text-left text-white">
-          <h1 className=" p-4 font-bold text-white text-4xl bg-ycs-pink rounded-lg">Events</h1>
-          <p className="mt-10 text-2xl text-white">
-            Each semester, the y/cs hosts dozens of events. Check here for our calendar to see
-            what&apos;s coming up!
-            <br />
-            <br />
-          </p>
-        </div>
-        <GoogleCalendar />
-      </div>
+    <div>
+      <SectionTitle
+        title="Events"
+        subtitle="Each semester, the y/cs hosts dozens of events. Check here for our calendar to see
+            whats coming up! You can click the button in the bottom right to add us to your Google Calendar"
+      />
+      <GoogleCalendar className=" mx-5 md:mx-32" />
     </div>
   );
 };
