c804257fcf18b84838ffaf10c6d0c18e098d1aaf
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-26 00:03:26 -0500

    Alter the pink blocks section and make it responsive

--- src/components/SolidColorBlock.tsx
+++ src/components/SolidColorBlock.tsx
@@ -6,12 +6,12 @@ interface Props {
 
 export const SolidColorBlock: React.FC<Props> = (props: Props) => {
   return (
-    <div className={`lg:w-1/3 sm:w-full m-8 bg-${props.color} rounded-lg`}>
+    <div className={`lg:w-1/3 md:w-2/3 bg-${props.color} rounded-lg text-center`}>
       <div className="p-6">
-        <div className="text-white lg:text-5xl md:text-3xl sm:text-3xl xs:text-2xl text-left font-bold mb-2">
+        <div className="text-white lg:text-5xl text-3xl font-bold mb-2">
           <p>{props.title}</p>
         </div>
-        <div className="text-white text-left lg:text-lg md:text-md sm:text-sm text-sm">
+        <div className="text-white lg:text-lg md:text-md sm:text-sm text-sm">
           <p>{props.desc}</p>
         </div>
       </div>
