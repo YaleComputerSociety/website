aa2570aeb12799928339509f3cdc2b019a38189f
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-27 16:21:39 -0500

    Refactor product card into its own component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { SectionTitle } from "@components/SectionTitle";
 import { PROJECTS } from "@data";
 import "@styles/App.css";
@@ -24,58 +25,18 @@ import { ReactElement, ReactNode } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+=======
+>>>>>>> e7167db (Refactor product card into its own component)
 import { SectionTitle } from "@components/SectionTitle";
-import { Project, PROJECTS } from "@data";
+import { PROJECTS } from "@data";
 import "@styles/App.css";
+import { ProductCard } from "@components/ProductCard";
 
-type ProductCardProps = {
-  product: Project;
-  roundedIcon?: boolean;
-};
-
-const ProductCard = ({ product, roundedIcon }: ProductCardProps) => {
-  const ConditionalWrapper = ({
-    condition,
-    wrapper,
-    children,
-  }: {
-    condition: boolean;
-    wrapper: (children: ReactNode) => ReactElement;
-    children: ReactNode;
-  }) => (condition ? wrapper(children) : children);
-  return (
-    <div
-      className={`flex flex-col relative ${product.link ? "project-card" : "project-card-no-link"}`}
-    >
-      <ConditionalWrapper
-        condition={product.link != null}
-        wrapper={(children: ReactNode) => (
-          <Link href={product.link!} style={{ textDecoration: "none", color: "inherit" }}>
-            {children}
-          </Link>
-        )}
-      >
-        <div className="w-40 h-40 bg-ycs-gray rounded-full absolute top-[-80px] left-1/2 -translate-x-1/2 flex justify-center items-center">
-          <Image
-            className={`${roundedIcon ? "rounded-full w-32 h-32" : "w-28 h-28"} ${product.name == "Yalies" && "left-7 absolute"}`}
-            src={product.image}
-            loading="lazy"
-            alt={product.name}
-          />
-        </div>
-        <div style={{ fontWeight: 500, fontSize: "25px", marginTop: "80px" }}>{product.name}</div>
-        <div style={{ fontWeight: 400, fontSize: "18px", margin: "40px 20px" }}>
-          {product.description}
-        </div>
-      </ConditionalWrapper>
-    </div>
-  );
-};
-
-interface ProductGridProps {
+interface Props {
   live: boolean;
 }
 
+<<<<<<< HEAD
 const ProductGrid = ({ live }: ProductGridProps) => (
   <div className="product-grid">
     {PROJECTS.filter((project) => project.live === live).map((project, i) => (
@@ -83,10 +44,24 @@ const ProductGrid = ({ live }: ProductGridProps) => (
     ))}
   </div>
 >>>>>>> d4869af (Rewrite the entire codebase in nextjs with stricter eslint and typescript settings)
+=======
+const ProductGrid = ({ live }: Props) => (
+  <>
+    <h2 className="text-3xl font-bold text-center mb-16">
+      {live ? "Live Products" : "Products in Development"}
+    </h2>
+    <div className="product-grid">
+      {PROJECTS.filter((project) => project.live === live).map((project, i) => (
+        <ProductCard product={project} roundedIcon={project.shouldIconBeRounded} key={i} />
+      ))}
+    </div>
+  </>
+>>>>>>> e7167db (Refactor product card into its own component)
 );
 
 const Products = () => {
   return (
+<<<<<<< HEAD
 <<<<<<< HEAD
     <>
       <SectionTitle
@@ -98,16 +73,21 @@ const Products = () => {
     </>
 =======
     <div className="product-container">
+=======
+    <>
+>>>>>>> e7167db (Refactor product card into its own component)
       <SectionTitle
         title="Products"
         subtitle="From a campus-wide student directory to your class-scheduling needs, we've got you covered."
       />
-      <h2 className="text-3xl font-bold my-20 text-center">Live Products</h2>
       <ProductGrid live />
-      <h2 className="text-3xl font-bold my-20 text-center">Products in Production</h2>
       <ProductGrid live={false} />
+<<<<<<< HEAD
     </div>
 >>>>>>> d4869af (Rewrite the entire codebase in nextjs with stricter eslint and typescript settings)
+=======
+    </>
+>>>>>>> e7167db (Refactor product card into its own component)
   );
 };
 
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 import { ReactNode, ReactElement } from "react";
 import Image from "next/image";
 
+<<<<<<< HEAD
 import nologoproduct1 from "../assets/products/logos/ycs.png";
 
+=======
+>>>>>>> e7167db (Refactor product card into its own component)
 import type { Project } from "@data";
 
 interface Props {
@@ -36,13 +39,22 @@ export const ProductCard = ({ product, roundedIcon }: Props) => {
         <div className="w-40 h-40 bg-ycs-gray rounded-full absolute top-[-80px] left-1/2 -translate-x-1/2 flex justify-center items-center">
           <Image
             className={`${roundedIcon ? "rounded-full w-32 h-32" : "w-28 h-28"} ${product.name == "Yalies" && "left-7 absolute"}`}
+<<<<<<< HEAD
             src={product.logo ? product.logo : nologoproduct1}
+=======
+            src={product.image}
+>>>>>>> e7167db (Refactor product card into its own component)
             loading="lazy"
             alt={product.name}
           />
         </div>
+<<<<<<< HEAD
         <div className="font-semibold text-xl md:text-3xl mt-16">{product.name}</div>
         <div className="font-normal text-md md:text-lg md:m-8 md:mt-4 m-4">
+=======
+        <div style={{ fontWeight: 500, fontSize: "25px", marginTop: "80px" }}>{product.name}</div>
+        <div style={{ fontWeight: 400, fontSize: "18px", margin: "40px 20px" }}>
+>>>>>>> e7167db (Refactor product card into its own component)
           {product.description}
         </div>
       </ConditionalWrapper>
