18f7a65dfe536e573a01e97636be694fbdbc2f36
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-02-27 15:54:31 -0500

    Add x margins to section title's subtitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -4,10 +4,12 @@ interface SectionTitleProps {
 }
 
 export const SectionTitle = ({ title: header, subtitle: subheader }: SectionTitleProps) => (
-  <div className="text-center py-10 max-w-xl m-auto">
-    <h1 className="bg-ycs-pink py-4 px-8 rounded-md text-2xl md:text-5xl xl:text-7xl font-bold inline-block">
-      {header}
-    </h1>
-    <h4 className="text-lg mt-5">{subheader}</h4>
+  <div className="flex flex-col mx-5">
+    <div className="text-center py-10 max-w-xl self-center">
+      <h1 className="bg-ycs-pink py-4 px-8 rounded-md text-2xl md:text-5xl xl:text-7xl font-bold inline-block">
+        {header}
+      </h1>
+      <h4 className="text-lg mt-5">{subheader}</h4>
+    </div>
   </div>
 );
