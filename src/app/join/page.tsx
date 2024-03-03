9e141c33a725b10eb893d67e4a880bebe594eb72
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-03-03 00:07:41 -0500

    Change section title style to match join us; remake join us title

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -3,6 +3,7 @@
 "use client";
 
 import { SectionTitle } from "@components/SectionTitle";
+<<<<<<< HEAD
 import { SolidColorBlock } from "@components/SolidColorBlock";
 import { TitleSubtitle } from "@components/TitleSubtitle";
 
@@ -112,6 +113,8 @@ export default Join;
 "use client";
 >>>>>>> b713017 (Refactor join page to conform to eslint)
 
+=======
+>>>>>>> 94d4678 (Change section title style to match join us; remake join us title)
 // import { SectionTitle } from "@components/SectionTitle";
 import { SolidColorBlock } from "@components/SolidColorBlock";
 
@@ -146,23 +149,50 @@ const ROLES = [
 
 const Join = () => {
   return (
-    <div className="ml-32 mr-32">
-      <h1 className="lg:text-8xl md:text-6xl sm:text-6xl text-6xl font-bold mt-10 text-ycs-pink mb-5">
-        &lt;Come build with us /&gt;
-      </h1>
+    <>
+      <SectionTitle
+        title="Build With Us"
+        subtitle="We are Yale's largest collective of software developers, product designers, and
+          computer science enthusiasts. We're excited that you're interested in joining
+          us!"
+      />
+      <div className="ml-32 mr-32">
+        <p className="lg:text-3xl md:text-xl sm:text-xl text-xl font-bold underline">Roles</p>
 
-      <p className="lg:text-4xl md:text-2xl sm:text-xl text-left mb-10">
-        We are Yale&apos;s largest collective of software developers, product designers, and
-        computer science enthusiasts. We&apos;re excited that you&apos;re interested in joining us!
-      </p>
+        <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-8">
+          {ROLES.map((r, index) => {
+            return <SolidColorBlock title={r.title} desc={r.desc} color="ycs-pink" key={index} />;
+          })}
+        </div>
 
-      <p className="lg:text-3xl md:text-xl sm:text-xl text-xl font-bold underline">Roles</p>
+        <div>
+          <p className="text-xl">
+            Applications to join y/cs open at the beginning of each semester. There are three phases
+            in the application process.
+            <br></br>
+            <br></br>
+            <span className="font-bold underline">1. Written Application</span>
+            <br></br>
+            Applicants are evaluated based on submitted short responses, resumé and past relevant
+            experiences.
+            <br></br>
+            <br></br>
+            <span className="font-bold underline">2. Interview</span>
+            <br></br>
+            Successful applicants are moved to an interview phase. Those applying for technical
+            roles likely recieve technical questions. Not all teams require interviews.
+            <br></br>
+            <br></br>
+            <span className="font-bold underline">3. Team Placement</span>
+            <br></br>
+            Based on your written application and interview you are placed on a y/cs team. Some
+            teams are competitive to join, while others are non-competitive.
+          </p>
+        </div>
 
-      <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-8">
-        {ROLES.map((r, index) => {
-          return <SolidColorBlock title={r.title} desc={r.desc} color="ycs-pink" key={index} />;
-        })}
+        {/* Put a button to apply here during application season */}
       </div>
+<<<<<<< HEAD
 
       <div>
         <p className="text-xl">
@@ -195,6 +225,9 @@ export default Join
 =======
       {/* Put a button to apply here during application season */}
     </div>
+=======
+    </>
+>>>>>>> 94d4678 (Change section title style to match join us; remake join us title)
   );
 };
 
