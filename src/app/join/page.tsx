cb0dfa694660cb209670a27ed957befc0ff61e64
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-03-03 00:07:41 -0500

    Change section title style to match join us; remake join us title

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { SectionTitle } from "@components/SectionTitle";
 // import { SectionTitle } from "@components/SectionTitle";
 import { SolidColorBlock } from "@components/SolidColorBlock";
 
@@ -34,51 +35,50 @@ const ROLES = [
 
 const Join = () => {
   return (
-    <div className="ml-32 mr-32">
-      <h1 className="lg:text-8xl md:text-6xl sm:text-6xl text-6xl font-bold mt-10 text-ycs-pink mb-5">
-        &lt;Come build with us /&gt;
-      </h1>
+    <>
+      <SectionTitle
+        title="Build With Us"
+        subtitle="We are Yale's largest collective of software developers, product designers, and
+          computer science enthusiasts. We're excited that you're interested in joining
+          us!"
+      />
+      <div className="ml-32 mr-32">
+        <p className="lg:text-3xl md:text-xl sm:text-xl text-xl font-bold underline">Roles</p>
 
-      <p className="lg:text-4xl md:text-2xl sm:text-xl text-left mb-10">
-        We are Yale&apos;s largest collective of software developers, product designers, and
-        computer science enthusiasts. We&apos;re excited that you&apos;re interested in joining us!
-      </p>
+        <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-8">
+          {ROLES.map((r, index) => {
+            return <SolidColorBlock title={r.title} desc={r.desc} color="ycs-pink" key={index} />;
+          })}
+        </div>
 
-      <p className="lg:text-3xl md:text-xl sm:text-xl text-xl font-bold underline">Roles</p>
+        <div>
+          <p className="text-xl">
+            Applications to join y/cs open at the beginning of each semester. There are three phases
+            in the application process.
+            <br></br>
+            <br></br>
+            <span className="font-bold underline">1. Written Application</span>
+            <br></br>
+            Applicants are evaluated based on submitted short responses, resumé and past relevant
+            experiences.
+            <br></br>
+            <br></br>
+            <span className="font-bold underline">2. Interview</span>
+            <br></br>
+            Successful applicants are moved to an interview phase. Those applying for technical
+            roles likely recieve technical questions. Not all teams require interviews.
+            <br></br>
+            <br></br>
+            <span className="font-bold underline">3. Team Placement</span>
+            <br></br>
+            Based on your written application and interview you are placed on a y/cs team. Some
+            teams are competitive to join, while others are non-competitive.
+          </p>
+        </div>
 
-      <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-8">
-        {ROLES.map((r, index) => {
-          return <SolidColorBlock title={r.title} desc={r.desc} color="ycs-pink" key={index} />;
-        })}
+        {/* Put a button to apply here during application season */}
       </div>
-
-      <div>
-        <p className="text-xl">
-          Applications to join y/cs open at the beginning of each semester. There are three phases
-          in the application process.
-          <br></br>
-          <br></br>
-          <span className="font-bold underline">1. Written Application</span>
-          <br></br>
-          Applicants are evaluated based on submitted short responses, resumé and past relevant
-          experiences.
-          <br></br>
-          <br></br>
-          <span className="font-bold underline">2. Interview</span>
-          <br></br>
-          Successful applicants are moved to an interview phase. Those applying for technical roles
-          likely recieve technical questions. Not all teams require interviews.
-          <br></br>
-          <br></br>
-          <span className="font-bold underline">3. Team Placement</span>
-          <br></br>
-          Based on your written application and interview you are placed on a y/cs team. Some teams
-          are competitive to join, while others are non-competitive.
-        </p>
-      </div>
-
-      {/* Put a button to apply here during application season */}
-    </div>
+    </>
   );
 };
 
