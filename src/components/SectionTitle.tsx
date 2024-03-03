d08360ad6e35c05a7bf7f59f920ce13f3bddf004
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-03-03 00:07:41 -0500

    Change section title style to match join us; remake join us title

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -5,11 +5,11 @@ interface SectionTitleProps {
 
 export const SectionTitle = ({ title: header, subtitle: subheader }: SectionTitleProps) => (
   <div className="flex flex-col mx-5">
-    <div className="text-center py-10 max-w-xl self-center">
-      <h1 className="bg-ycs-pink py-4 px-8 rounded-md text-2xl md:text-5xl xl:text-7xl font-bold inline-block">
-        {header}
+    <div className="text-center py-10 self-center">
+      <h1 className="md:text-8xl sm:text-7xl text-[2.3rem] font-bold text-ycs-pink mb-5">
+        &lt;{header} /&gt;
       </h1>
-      <h4 className="text-lg mt-5">{subheader}</h4>
+      <h4 className="md:text-3xl sm:text-2xl text-lg text-center mb-10 md:mx-16">{subheader}</h4>
     </div>
   </div>
 );
