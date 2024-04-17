211dfa5458536be3f61fe64f752278a581b7f72c
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-04-17 12:35:54 -0400

    Make product header responsive

--- src/components/GeneralProductHeader.tsx
+++ src/components/GeneralProductHeader.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import { PROJECTS } from "@data";
+
 interface GeneralProductHeaderProps {
   productName: string;
   productColor: string;
@@ -18,61 +20,40 @@ export const GeneralProductHeader = ({
   buttonDescription,
   isWeb,
 }: GeneralProductHeaderProps) => {
+  const logo = PROJECTS.find((project) => project.name === productName)?.image;
+
   return (
-    <div
-      style={{
-        background: "#1F232C",
-        padding: "20px 0",
-        height: "100vh",
-        display: "flex",
-      }}
-    >
-      <div className="section-text-container">
-        <div className="text-container" style={{ width: "60%" }}>
-          <div style={{ display: "flex", alignItems: "center", width: "100%" }}>
-            <div
-              style={{
-                fontWeight: 700,
-                color: "white",
-                fontSize: "44px",
-                float: "left",
-                display: "inline-block",
-              }}
-            >
-              {productName}
-            </div>
+    <div className="flex h-[85vh] flex-col md:flex-row items-center">
+      <div className="flex flex-col gap-4 px-4 md:px-0 md:pl-12 pt-8 md:pt-0">
+        <div className="flex flex-row items-center self-center">
+          <div className="font-bold text-3xl md:text-4xl lg:text-5xl">{productName}</div>
+          {logo != null && (
             <Image
-              className="w-20 h-20 rounded-lg"
-              src={require(`../assets/products/${productName}/productlogo.png`)}
+              className="rounded-lg ml-6"
+              src={logo}
               alt={productName}
-              style={{ marginLeft: "1rem", display: "inline-block" }}
+              width={70}
+              height={70}
+              sizes="(max-width: 768px) 60px, 70px"
+              layout="responsive"
             />
-          </div>
-          <div
-            style={{
-              fontWeight: 400,
-              color: "white",
-              fontSize: "24px",
-              textAlign: "left",
-            }}
-          >
-            {productSlogan}
-          </div>
-          <Link
-            href={productLink}
-            className="visit-button"
-            style={{ backgroundColor: productColor }}
-          >
-            {buttonDescription}
-          </Link>
+          )}
         </div>
+        <div className="text-lg md:text-2xl text-center md:text-left">{productSlogan}</div>
+        <Link
+          href={productLink}
+          className="md:mt-5 mx-auto py-4 px-8 md:px-12 rounded-full text-center font-medium text-xl md:text-2xl"
+          style={{ backgroundColor: productColor }}
+        >
+          {buttonDescription}
+        </Link>
       </div>
-      <div className="section-image-container">
+      <div className="">
         {isWeb ? (
           <Image
             src={require(`../assets/products/${productName}/headerimage.png`)}
-            style={{ width: "85%" }}
             alt="Product Main Screenshot"
+            className="px-6 md:px-16 pt-14 md:pt-0 flex-or"
           />
         ) : (
           <>
