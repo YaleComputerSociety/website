43186b540ca8ca93bf4daf23b6e18d1bdcac1346
Tyler Schroder <11656073+Redorhcs@users.noreply.github.com>
2024-07-31 02:56:32 +0000

    remove extra info, basic page ready

--- src/app/newstudents/page.tsx
+++ src/app/newstudents/page.tsx
@@ -1,7 +1,7 @@
 import { SectionTitle } from "@components/SectionTitle";
-import { InfoCard } from "@components/InfoCard";
-import { STORIES, TODAY_STATISTICS } from "@data";
-import { Sponsors } from "@components/Sponsors";
+//import { InfoCard } from "@components/InfoCard";
+//import { STORIES, TODAY_STATISTICS } from "@data";
+//import { Sponsors } from "@components/Sponsors";
 import { TitleSubtitle } from "@components/TitleSubtitle";
 
 const NewStudents = () => {
@@ -19,36 +19,20 @@ const NewStudents = () => {
         }}
       >
         <text>
-          Y/CS products such as Yalies and Coursetable respect Yale's matriculation process and access is not granted until you are set to an active student in Yale's system.
+          Y/CS products such as Yalies and Coursetable respect Yale's matriculation process;
+          Access to products are not granted until you are set to an active student in Yale's system.
           You are considered an incoming student from the time you accept Yale's offer of admission to early August. 
           In August, this status will change to active and you should log out and log in again on CourseTable to refresh your authentication status.
           The Y/CS is unable to manually grant access or change your student status, as this is all handled centrally by the University Registrar's Office.
         </text>
       </div>
-
-      <TitleSubtitle
+   <TitleSubtitle
         title="While You're Waiting"
         subtitle="Learn what you can use to get the most out of your Yale Experience!"
         className="mb-10 mt-32"
       />
 
-      <div className="flex flex-col md:flex-row pt-10 gap-16 md:gap-4 mx-10">
-        {STORIES.map((story, i) => (
-          <InfoCard data={story.data} description={story.description} key={i} />
-        ))}
-      </div>
-
-      <TitleSubtitle title="Today" subtitle="By the Numbers" className="mb-10 mt-32" />
-
-      <div className="flex flex-col md:flex-row pt-10 gap-16 md:gap-4 mx-10">
-        {TODAY_STATISTICS.map((story, i) => (
-          <InfoCard data={story.data} description={story.description} key={i} />
-        ))}
-      </div>
-
-      <Sponsors />
-      <div className="mt-20" />
-    </div>
+    </div> 
   );
 };
 
